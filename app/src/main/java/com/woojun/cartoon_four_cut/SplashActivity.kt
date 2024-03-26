9804ae1bc0db6a530afcd37a7c99d94b63597ab4
JangWoojun <woojun0107@naver.com>
2024-03-26 11:48:27 +0900

    feat: 글자 애니메이션 효과 설정

--- app/src/main/java/com/woojun/cartoon_four_cut/SplashActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/SplashActivity.kt
@@ -1,8 +1,13 @@
 package com.woojun.cartoon_four_cut
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.woojun.cartoon_four_cut.databinding.ActivitySplashBinding
+
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
@@ -10,5 +15,29 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val fadeIn = ObjectAnimator.ofFloat(binding.logo1, "alpha", 0f, 1f)
+            fadeIn.duration = 500
+            fadeIn.start()
+        }, 500)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val fadeIn = ObjectAnimator.ofFloat(binding.logo2, "alpha", 0f, 1f)
+            fadeIn.duration = 500
+            fadeIn.start()
+        }, 1000)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val fadeIn = ObjectAnimator.ofFloat(binding.logo3, "alpha", 0f, 1f)
+            fadeIn.duration = 500
+            fadeIn.start()
+        }, 1500)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val fadeIn = ObjectAnimator.ofFloat(binding.logo4, "alpha", 0f, 1f)
+            fadeIn.duration = 500
+            fadeIn.start()
+        }, 2000)
     }
 }

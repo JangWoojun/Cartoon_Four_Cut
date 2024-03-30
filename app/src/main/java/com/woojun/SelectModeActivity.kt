6d38f5da4e709ba6077132411fac769584b68c12
JangWoojun <woojun0107@naver.com>
2024-03-31 03:08:06 +0900

    feat: 버튼들 이동 효과 추가

--- app/src/main/java/com/woojun/SelectModeActivity.kt
+++ app/src/main/java/com/woojun/SelectModeActivity.kt
@@ -1,12 +1,25 @@
 package com.woojun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.woojun.cartoon_four_cut.R
+import com.woojun.cartoon_four_cut.CameraActivity
+import com.woojun.cartoon_four_cut.PhotoActivity
+import com.woojun.cartoon_four_cut.databinding.ActivitySelectModeBinding
 
 class SelectModeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySelectModeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select_mode)
+        binding = ActivitySelectModeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.takePhotoButton.setOnClickListener {
+            startActivity(Intent(this@SelectModeActivity, CameraActivity::class.java))
+        }
+
+        binding.galleryButton.setOnClickListener {
+            startActivity(Intent(this@SelectModeActivity, PhotoActivity::class.java))
+        }
     }
 }

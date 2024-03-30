8a7c7ddf54ab1b8f0d03dd4817858e202c6f370a
JangWoojun <woojun0107@naver.com>
2024-03-30 20:56:32 +0900

    feat: SelectModeActivity 추가

---
+++ app/src/main/java/com/woojun/SelectModeActivity.kt
@@ -0,0 +1,12 @@
+package com.woojun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.woojun.cartoon_four_cut.R
+
+class SelectModeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_mode)
+    }
+}

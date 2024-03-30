2a682f4efa39ce809b1fb914f682dd01f0f25d0d
JangWoojun <woojun0107@naver.com>
2024-03-30 20:57:39 +0900

    feat: takePhotoButton 클릭 시 효과 추가

--- app/src/main/java/com/woojun/cartoon_four_cut/MainActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.woojun.cartoon_four_cut
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.woojun.SelectModeActivity
+import com.woojun.cartoon_four_cut.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.takePhotoButton.setOnClickListener {
+            startActivity(Intent(this@MainActivity, SelectModeActivity::class.java))
+        }
     }
 }

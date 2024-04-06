0ec5c149ad044c98391377a3c806545078dbc610
JangWoojun <woojun0107@naver.com>
2024-04-07 04:11:24 +0900

    feat: FilterActivity 추가

---
+++ app/src/main/java/com/woojun/cartoon_four_cut/FilterActivity.kt
@@ -0,0 +1,14 @@
+package com.woojun.cartoon_four_cut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.woojun.cartoon_four_cut.databinding.ActivityFilterBinding
+
+class FilterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFilterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFilterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}

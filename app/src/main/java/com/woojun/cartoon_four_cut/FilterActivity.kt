ade44af7362a5b52746dcf7e73759b04e6435a45
JangWoojun <woojun0107@naver.com>
2024-04-07 09:52:15 +0900

    feat: FilterActivity ViewPager2 구현

--- app/src/main/java/com/woojun/cartoon_four_cut/FilterActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/FilterActivity.kt
@@ -1,8 +1,14 @@
 package com.woojun.cartoon_four_cut
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.woojun.cartoon_four_cut.BitmapData.getImage1
+import com.woojun.cartoon_four_cut.BitmapData.getImage2
+import com.woojun.cartoon_four_cut.BitmapData.getImage3
+import com.woojun.cartoon_four_cut.BitmapData.getImage4
 import com.woojun.cartoon_four_cut.databinding.ActivityFilterBinding
+
 
 class FilterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFilterBinding
@@ -10,5 +16,16 @@ class FilterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityFilterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (getImage1() != null && getImage2() != null && getImage3() != null && getImage4() != null) {
+            binding.viewPager.adapter = ViewPagerAdapter()
+            binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+            binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+
+                }
+            })
+        }
     }
 }

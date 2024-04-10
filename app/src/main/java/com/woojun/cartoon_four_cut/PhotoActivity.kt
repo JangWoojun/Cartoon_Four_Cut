ecf4554b4f84be6e35c5fc1cd570a128bf23ae38
JangWoojun <woojun0107@naver.com>
2024-04-11 03:56:10 +0900

    feat: PhotoActivity 이미지 임시 저장 로직 추가
    Uri를 가져온 뒤 bitmap으로 변환 후 임시 저장 로직 추가

--- app/src/main/java/com/woojun/cartoon_four_cut/PhotoActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/PhotoActivity.kt
@@ -1,48 +1,66 @@
 package com.woojun.cartoon_four_cut
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.woojun.cartoon_four_cut.databinding.ActivityPhotoBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 class PhotoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPhotoBinding
     private var selectFrame: Int = 1
+    private val imageList: MutableList<Pair<Int, Uri>> = mutableListOf()
 
     private var pickMedia = registerForActivityResult(
-        ActivityResultContracts.PickVisualMedia()) { uri ->
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri ->
         uri.let {
-            when (selectFrame) {
-                1 -> {
-                    Glide.with(this@PhotoActivity)
-                        .load(it)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image1)
-                }
-                2 -> {
-                    Glide.with(this@PhotoActivity)
-                        .load(it)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image2)
-                }
-                3 -> {
-                    Glide.with(this@PhotoActivity)
-                        .load(it)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image3)
-                }
-                4 -> {
-                    Glide.with(this@PhotoActivity)
-                        .load(it)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image4)
+            if (uri != null) {
+                when (selectFrame) {
+                    1 -> {
+                        Glide.with(this@PhotoActivity)
+                            .load(it)
+                            .centerCrop()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .into(binding.image1)
+                        imageList.add(Pair(selectFrame, uri))
+                    }
+                    2 -> {
+                        Glide.with(this@PhotoActivity)
+                            .load(it)
+                            .centerCrop()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .into(binding.image2)
+                        imageList.add(Pair(selectFrame, uri))
+                    }
+                    3 -> {
+                        Glide.with(this@PhotoActivity)
+                            .load(it)
+                            .centerCrop()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .into(binding.image3)
+                        imageList.add(Pair(selectFrame, uri))
+                    }
+                    4 -> {
+                        Glide.with(this@PhotoActivity)
+                            .load(it)
+                            .centerCrop()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .into(binding.image4)
+                        imageList.add(Pair(selectFrame, uri))
+                    }
                 }
             }
         }
@@ -76,7 +94,24 @@ class PhotoActivity : AppCompatActivity() {
         }
 
         binding.selectButton.setOnClickListener {
-
+            if (imageList.size == 4) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    imageList.forEach {
+                        val bitmap = getBitmap(it.second)
+                        when (it.first) {
+                            1 -> BitmapData.setImage1(bitmap)
+                            2 -> BitmapData.setImage2(bitmap)
+                            3 -> BitmapData.setImage3(bitmap)
+                            4 -> BitmapData.setImage4(bitmap)
+                        }
+                    }
+                    withContext(Dispatchers.Main) {
+                        startActivity(Intent(this@PhotoActivity, FilterActivity::class.java))
+                    }
+                }
+            } else {
+                Toast.makeText(this@PhotoActivity, "모든 사진을 선택해주세요", Toast.LENGTH_SHORT).show()
+            }
         }
 
 
@@ -89,5 +124,14 @@ class PhotoActivity : AppCompatActivity() {
     override fun onBackPressed() {
         super.onBackPressed()
         overridePendingTransition(R.anim.anim_slide_in_from_left_fade_in, R.anim.anim_fade_out)
+    }
+
+    private fun getBitmap(imageUri: Uri): Bitmap {
+        return ImageDecoder.decodeBitmap(
+            ImageDecoder.createSource(
+                contentResolver,
+                imageUri
+            )
+        )
     }
 }

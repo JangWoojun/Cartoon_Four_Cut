3d2b83639b2efaf13df7e3ee3eadb0f072039661
JangWoojun <woojun0107@naver.com>
2024-04-11 05:03:49 +0900

    feat: HomePhotoFrame 추가

---
+++ app/src/main/java/com/woojun/cartoon_four_cut/HomePhotoFrame.kt
@@ -0,0 +1,11 @@
+package com.woojun.cartoon_four_cut
+
+import android.graphics.Bitmap
+
+data class HomePhotoFrame (
+    val image1: Bitmap,
+    val image2: Bitmap,
+    val image3: Bitmap,
+    val image4: Bitmap,
+    val date: String
+)

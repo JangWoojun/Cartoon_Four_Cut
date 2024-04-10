48383fcc03176af44014c299b03a20476bcbf397
JangWoojun <woojun0107@naver.com>
2024-04-11 05:04:26 +0900

    feat: HomePhotoFrameAdapter 연결

--- app/src/main/java/com/woojun/cartoon_four_cut/MainActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/MainActivity.kt
@@ -3,6 +3,7 @@ package com.woojun.cartoon_four_cut
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.woojun.cartoon_four_cut.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +16,10 @@ class MainActivity : AppCompatActivity() {
         binding.takePhotoButton.setOnClickListener {
             startActivity(Intent(this@MainActivity, SelectModeActivity::class.java))
         }
+
+        val adapter = HomePhotoFrameAdapter(mutableListOf())
+
+        binding.photoRecyclerView.adapter = adapter
+        binding.photoRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
     }
 }

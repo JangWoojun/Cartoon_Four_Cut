b77960ad6b531b6cd511f5a749222404f1387dbc
JangWoojun <woojun0107@naver.com>
2024-04-12 17:32:24 +0900

    feat: glide:compiler 종속성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     api("com.otaliastudios:cameraview:2.7.2")
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 
     implementation("androidx.viewpager2:viewpager2:1.0.0")
 

5d3250ba9c796cdee5d284871602aac42539f280
JangWoojun <woojun0107@naver.com>
2024-05-05 21:05:28 +0900

    feat: FilterTypes 필터들 추가

--- app/src/main/java/com/woojun/cartoon_four_cut/FilterTypes.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/FilterTypes.kt
@@ -1,5 +1,22 @@
 package com.woojun.cartoon_four_cut
 
-enum class FilterTypes {
-    None
+enum class FilterTypes(val filterName: String) {
+    None("필터 X"),
+    Test1("필터 O - Test1"),
+    Test2("필터 O - Test2"),
+    Test3("필터 O - Test3"),
+    Test4("필터 O - Test4"),
+    Disney("Disney A.I 필터"),
+    Anime("Anime A.I 필터"),
+    Comics("Comics A.I 필터"),
+    Flat2d("Flat2d A.I 필터"),
+    Real3d("Real3d A.I 필터");
+    companion object {
+        fun fromFilterName(filterType: FilterTypes): String {
+            return filterType.filterName
+        }
+        fun fromNameFilter(name: String): FilterTypes {
+            return entries.find { it.filterName == name }!!
+        }
+    }
 }

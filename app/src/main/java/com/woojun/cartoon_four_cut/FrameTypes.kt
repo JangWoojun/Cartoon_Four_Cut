38c94313aff6437b1e4c40edc378a0aa87f0dff1
JangWoojun <woojun0107@naver.com>
2024-05-05 21:04:45 +0900

    feat: FrameTypes 추가

--- app/src/main/java/com/woojun/cartoon_four_cut/FrameTypes.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/FrameTypes.kt
@@ -1,5 +1,14 @@
 package com.woojun.cartoon_four_cut
 
-enum class FrameTypes {
-    None
+enum class FrameTypes (frameName: String) {
+    None("프레임 X"),
+    Test1("프레임 O - Test1"),
+    Test2("프레임 O - Test2"),
+    Test3("프레임 O - Test3"),
+    Test4("프레임 O - Test4");
+    companion object {
+        fun fromFrameName(frameName: FrameTypes): String {
+            return FrameTypes.entries.find {it.name == frameName.name}?.name!!
+        }
+    }
 }

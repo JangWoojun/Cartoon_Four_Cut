776171b95181e442aa43f876efd5a70043e13174
JangWoojun <woojun0107@naver.com>
2024-05-22 13:24:46 +0900

    feat: 이미지 다운로드 로직 구현

--- app/src/main/java/com/woojun/cartoon_four_cut/HomePhotoFrameAdapter.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/HomePhotoFrameAdapter.kt
@@ -1,18 +1,27 @@
 package com.woojun.cartoon_four_cut
 
 import android.app.Dialog
+import android.content.ContentValues
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.provider.MediaStore
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.woojun.cartoon_four_cut.databinding.HomePhotoFrameItemBinding
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 
 class HomePhotoFrameAdapter(private val photoFrameList: MutableList<HomePhotoFrame>): RecyclerView.Adapter<HomePhotoFrameAdapter.HomePhotoFrameViewHolder>() {
 
@@ -21,6 +30,10 @@ class HomePhotoFrameAdapter(private val photoFrameList: MutableList<HomePhotoFra
         return HomePhotoFrameViewHolder(binding).also { handler->
             binding.deleteButton.setOnClickListener {
                 showDialog(binding.root.context, handler.position)
+            }
+            binding.downloadButton.setOnClickListener {
+                val bitmap = viewToImage(binding.mainFrame)
+                saveImageOnAboveAndroidQ(binding.root.context, bitmap)
             }
         }
     }
@@ -85,6 +98,56 @@ class HomePhotoFrameAdapter(private val photoFrameList: MutableList<HomePhotoFra
         }
 
         dialog.show()
+    }
+
+    private fun viewToImage(view: View): Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) bgDrawable.draw(canvas) else canvas.drawColor(Color.WHITE)
+        view.draw(canvas)
+        return returnedBitmap
+    }
+
+    private fun saveImageOnAboveAndroidQ(context: Context, bitmap: Bitmap) {
+        val fileName = System.currentTimeMillis().toString() + ".png"
+
+        val contentValues = ContentValues()
+        contentValues.apply {
+            put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/ImageSave")
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+            put(MediaStore.Images.Media.IS_PENDING, 1)
+        }
+
+        val uri = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+        try {
+            if(uri != null) {
+                context.contentResolver.openFileDescriptor(uri, "w", null).use {
+                    if(it != null) {
+                        val fos = FileOutputStream(it.fileDescriptor)
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
+                        fos.close()
+
+                        contentValues.clear()
+                        contentValues.put(MediaStore.Images.Media.IS_PENDING, 0)
+                        context.contentResolver.update(uri, contentValues, null, null)
+                        Toast.makeText(context, "이미지 저장이 완료되었습니다", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, "이미지 저장을 실패하였습니다", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(context, "이미지 저장을 실패하였습니다", Toast.LENGTH_SHORT).show()
+            }
+        } catch(e: FileNotFoundException) {
+            Toast.makeText(context, "이미지 저장을 실패하였습니다", Toast.LENGTH_SHORT).show()
+        } catch (e: IOException) {
+            Toast.makeText(context, "이미지 저장을 실패하였습니다", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Toast.makeText(context, "이미지 저장을 실패하였습니다", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }

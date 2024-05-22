096cdac256cddff6f49a0800db09c50adc523359
JangWoojun <woojun0107@naver.com>
2024-05-22 12:02:33 +0900

    feat: getAuthId 추가

---
+++ app/src/main/java/com/woojun/cartoon_four_cut/data/AuthResponse.kt
@@ -0,0 +1,6 @@
+package com.woojun.cartoon_four_cut.data
+
+data class AuthResponse(
+    val id: String,
+    val statusCode: Int
+)
--- app/src/main/java/com/woojun/cartoon_four_cut/network/RetrofitAPI.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/network/RetrofitAPI.kt
@@ -1,6 +1,12 @@
 package com.woojun.cartoon_four_cut.network
 
+import com.woojun.cartoon_four_cut.BuildConfig
+import com.woojun.cartoon_four_cut.data.AuthResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
 
 interface RetrofitAPI {
-
+    @GET("${BuildConfig.baseUrl}auth")
+    fun getAuthId(): Call<AuthResponse>
 }

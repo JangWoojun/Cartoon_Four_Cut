d81b5c3a0233fc6bcffc8bb5587a65e237588368
JangWoojun <woojun0107@naver.com>
2024-05-23 23:19:24 +0900

    feat: getFrame 요청 추가

--- app/src/main/java/com/woojun/cartoon_four_cut/network/RetrofitAPI.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/network/RetrofitAPI.kt
@@ -2,6 +2,7 @@ package com.woojun.cartoon_four_cut.network
 
 import com.woojun.cartoon_four_cut.BuildConfig
 import com.woojun.cartoon_four_cut.data.AuthResponse
+import com.woojun.cartoon_four_cut.data.FrameResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
@@ -9,4 +10,7 @@ import retrofit2.http.GET
 interface RetrofitAPI {
     @GET("${BuildConfig.baseUrl}auth")
     fun getAuthId(): Call<AuthResponse>
+
+    @GET("${BuildConfig.baseUrl}frame")
+    fun getFrame(): Call<FrameResponse>
 }

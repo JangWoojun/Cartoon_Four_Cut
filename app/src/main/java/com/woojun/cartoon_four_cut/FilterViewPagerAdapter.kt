2bed68891fdee245478b2938c114015a8cf5da83
JangWoojun <woojun0107@naver.com>
2024-05-23 23:14:03 +0900

    feat: ViewHolder 이름 변경 및 이미지 binding 로직 변경

--- app/src/main/java/com/woojun/cartoon_four_cut/FilterViewPagerAdapter.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/FilterViewPagerAdapter.kt
@@ -5,72 +5,58 @@ import android.view.ViewGroup
 import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.woojun.cartoon_four_cut.Utils.dpToPx
 import com.woojun.cartoon_four_cut.databinding.PhotoFrameLayoutBinding
 
-class FilterViewPagerAdapter : RecyclerView.Adapter<FilterViewPagerAdapter.ViewPagerAdapter>() {
-    private val filterList = FilterTypes.entries.toTypedArray()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerAdapter {
+class FilterViewPagerAdapter(private val filterItemList: MutableList<FilterItem>) : RecyclerView.Adapter<FilterViewPagerAdapter.ViewPagerHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerHolder {
         val binding = PhotoFrameLayoutBinding.inflate(
             LayoutInflater.from(parent.context),
             parent, false
         )
 
-        return ViewPagerAdapter(binding)
+        return ViewPagerHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewPagerAdapter, position: Int) {
-        holder.bind(filterList[position])
+    override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
+        holder.bind(filterItemList[position])
     }
 
     override fun getItemCount(): Int {
-        return filterList.size
+        return filterItemList.size
     }
 
 
-    class ViewPagerAdapter(private val binding: PhotoFrameLayoutBinding) :
+    class ViewPagerHolder(private val binding: PhotoFrameLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        private val image1 = BitmapData.getImage1()
-        private val image2 = BitmapData.getImage2()
-        private val image3 = BitmapData.getImage3()
-        private val image4 = BitmapData.getImage4()
-
-        fun bind(filterType: FilterTypes) {
+        fun bind(filterItem: FilterItem) {
             binding.root.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 marginStart = binding.root.context.dpToPx(24f).toInt()
                 marginEnd = binding.root.context.dpToPx(24f).toInt()
             }
 
-            when (filterType) {
-                FilterTypes.None -> {
-                    Glide.with(binding.root.context)
-                        .load(image1)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image1)
+            binding.filterText.text = filterItem.name
 
-                    Glide.with(binding.root.context)
-                        .load(image2)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image2)
+            Glide.with(binding.root.context)
+                .load(filterItem.images[0])
+                .centerCrop()
+                .into(binding.image1)
 
-                    Glide.with(binding.root.context)
-                        .load(image3)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image3)
+            Glide.with(binding.root.context)
+                .load(filterItem.images[1])
+                .centerCrop()
+                .into(binding.image2)
 
-                    Glide.with(binding.root.context)
-                        .load(image4)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image4)
-                }
-            }
+            Glide.with(binding.root.context)
+                .load(filterItem.images[2])
+                .centerCrop()
+                .into(binding.image3)
+
+            Glide.with(binding.root.context)
+                .load(filterItem.images[3])
+                .centerCrop()
+                .into(binding.image4)
         }
     }
 
--- app/src/main/java/com/woojun/cartoon_four_cut/FrameViewPagerAdapter.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/FrameViewPagerAdapter.kt
@@ -2,69 +2,58 @@ package com.woojun.cartoon_four_cut
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.woojun.cartoon_four_cut.Utils.dpToPx
 import com.woojun.cartoon_four_cut.databinding.PhotoFrameLayoutBinding
 
-class FrameViewPagerAdapter : RecyclerView.Adapter<FrameViewPagerAdapter.ViewPagerAdapter>() {
-    private val frameList = FrameTypes.entries.toTypedArray()
+class FrameViewPagerAdapter(private val frameItemList: MutableList<FrameItem>) : RecyclerView.Adapter<FrameViewPagerAdapter.ViewPagerHolder>()  {
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerAdapter {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerHolder {
         val binding = PhotoFrameLayoutBinding.inflate(
             LayoutInflater.from(parent.context),
             parent, false
         )
-
-        return ViewPagerAdapter(binding)
+        return ViewPagerHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewPagerAdapter, position: Int) {
-        holder.bind(frameList[position])
+    override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
+        holder.bind(frameItemList[position])
     }
 
     override fun getItemCount(): Int {
-        return frameList.size
+        return frameItemList.size
     }
 
-
-    class ViewPagerAdapter(private val binding: PhotoFrameLayoutBinding) :
+    class ViewPagerHolder(private val binding: PhotoFrameLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        private val image1 = BitmapData.getImage1()
-        private val image2 = BitmapData.getImage2()
-        private val image3 = BitmapData.getImage3()
-        private val image4 = BitmapData.getImage4()
-
-        fun bind(frameTypes: FrameTypes) {
-            when (frameTypes) {
-                FrameTypes.None -> {
-                    Glide.with(binding.root.context)
-                        .load(image1)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image1)
-
-                    Glide.with(binding.root.context)
-                        .load(image2)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image2)
-
-                    Glide.with(binding.root.context)
-                        .load(image3)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image3)
-
-                    Glide.with(binding.root.context)
-                        .load(image4)
-                        .centerCrop()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(binding.image4)
-                }
+        fun bind(frameItem: FrameItem) {
+            binding.root.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                marginStart = binding.root.context.dpToPx(24f).toInt()
+                marginEnd = binding.root.context.dpToPx(24f).toInt()
             }
+
+            Glide.with(binding.root.context)
+                .load(frameItem.images[0])
+                .centerCrop()
+                .into(binding.image1)
+
+            Glide.with(binding.root.context)
+                .load(frameItem.images[1])
+                .centerCrop()
+                .into(binding.image2)
+
+            Glide.with(binding.root.context)
+                .load(frameItem.images[2])
+                .centerCrop()
+                .into(binding.image3)
+
+            Glide.with(binding.root.context)
+                .load(frameItem.images[3])
+                .centerCrop()
+                .into(binding.image4)
         }
     }
 

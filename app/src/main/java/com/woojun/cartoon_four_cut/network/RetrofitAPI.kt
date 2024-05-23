1c18173ff727d76d8be0e0f2f4470afb40150804
JangWoojun <woojun0107@naver.com>
2024-05-23 23:30:35 +0900

    feat: postUpload 요청 추가

--- app/src/main/java/com/woojun/cartoon_four_cut/network/RetrofitAPI.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/network/RetrofitAPI.kt
@@ -3,8 +3,16 @@ package com.woojun.cartoon_four_cut.network
 import com.woojun.cartoon_four_cut.BuildConfig
 import com.woojun.cartoon_four_cut.data.AuthResponse
 import com.woojun.cartoon_four_cut.data.FrameResponse
+import com.woojun.cartoon_four_cut.data.UploadRequest
+import com.woojun.cartoon_four_cut.data.UploadResponse
+import okhttp3.MultipartBody
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 
 
 interface RetrofitAPI {
@@ -13,4 +21,12 @@ interface RetrofitAPI {
 
     @GET("${BuildConfig.baseUrl}frame")
     fun getFrame(): Call<FrameResponse>
+
+    @Multipart
+    @POST("${BuildConfig.baseUrl}upload")
+    fun postUpload(
+        @Header("Header") header: UploadRequest,
+        @Part body: MultipartBody.Part,
+    ): Call<UploadResponse>
+
 }

ef6cb81f484b27811e1ed116c4d51e8150517c18
JangWoojun <woojun0107@naver.com>
2024-05-24 11:12:42 +0900

    feat: 필터 로직 구현

--- app/src/main/java/com/woojun/cartoon_four_cut/FilterActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/FilterActivity.kt
@@ -2,51 +2,160 @@ package com.woojun.cartoon_four_cut
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.viewpager2.widget.ViewPager2
+import com.woojun.cartoon_four_cut.BitmapData.getImage1
+import com.woojun.cartoon_four_cut.BitmapData.getImage2
+import com.woojun.cartoon_four_cut.BitmapData.getImage3
+import com.woojun.cartoon_four_cut.BitmapData.getImage4
+import com.woojun.cartoon_four_cut.BitmapData.setImage1
+import com.woojun.cartoon_four_cut.BitmapData.setImage2
+import com.woojun.cartoon_four_cut.BitmapData.setImage3
+import com.woojun.cartoon_four_cut.BitmapData.setImage4
 import com.woojun.cartoon_four_cut.Utils.dpToPx
 import com.woojun.cartoon_four_cut.databinding.ActivityFilterBinding
+import com.woojun.cartoon_four_cut.network.RetrofitAPI
+import com.woojun.cartoon_four_cut.network.RetrofitClient
+import com.zomato.photofilters.SampleFilters
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class FilterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFilterBinding
+
+    private var filterIndex = 0
+
+    companion object {
+        init {
+            System.loadLibrary("NativeImageProcessor")
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFilterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val isBitmapNotNull = (BitmapData.getImage1() != null && BitmapData.getImage2() != null && BitmapData.getImage3() != null && BitmapData.getImage4() != null)
+        val isBitmapNotNull = (getImage1() != null && getImage2() != null && getImage3() != null && getImage4() != null)
 
-        if (isBitmapNotNull) {
-            binding.viewPager.apply {
-                adapter = FilterViewPagerAdapter()
+        generateFilterItemList { list ->
+            val adapter = FilterViewPagerAdapter(list)
 
-                offscreenPageLimit = 1
+            if (isBitmapNotNull) {
+                binding.viewPager.apply {
+                    this.adapter = adapter
+                    offscreenPageLimit = 3
 
-                val pageMargin = this@FilterActivity.dpToPx(8f)
-                val offset = this@FilterActivity.dpToPx(24f)
-                setPageTransformer { page, position ->
-                    val pageOffset = position * -(2 * offset + pageMargin)
-                    if (ViewCompat.getLayoutDirection(binding.viewPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
-                        page.translationX = -pageOffset
-                    } else {
-                        page.translationX = pageOffset
-                    }
-                }
-
-                registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-                    override fun onPageSelected(position: Int) {
-                        when (FilterTypes.entries[position]) {
-                            FilterTypes.None -> {}
+                    val pageMargin = this@FilterActivity.dpToPx(8f)
+                    val offset = this@FilterActivity.dpToPx(16f)
+                    setPageTransformer { page, position ->
+                        val pageOffset = position * -(2 * offset + pageMargin)
+                        if (ViewCompat.getLayoutDirection(binding.viewPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
+                            page.translationX = -pageOffset
+                        } else {
+                            page.translationX = pageOffset
                         }
                     }
-                })
-            }
+                    registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                        override fun onPageSelected(position: Int) {
+                            filterIndex = position
+                        }
+                    })
+                }
 
-            binding.selectButton.setOnClickListener {
-                startActivity(Intent(this@FilterActivity, FrameActivity::class.java))
+                binding.selectButton.setOnClickListener {
+                    val intent = Intent(this@FilterActivity, FrameActivity::class.java)
+
+                    if (list[filterIndex].isAi) {
+                        intent.putExtra("isAi", list[filterIndex].name)
+                    } else {
+                        setImage1(list[filterIndex].images[0])
+                        setImage2(list[filterIndex].images[1])
+                        setImage3(list[filterIndex].images[2])
+                        setImage4(list[filterIndex].images[3])
+                    }
+
+                    overridePendingTransition(R.anim.anim_slide_in_from_right_fade_in, R.anim.anim_fade_out)
+                    startActivity(intent)
+                }
             }
         }
+
     }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.anim_slide_in_from_left_fade_in, R.anim.anim_fade_out)
+    }
+
+    private fun generateFilterItemList(callback: (List<FilterItem>) -> Unit) {
+        val filterList = mutableListOf(
+            null,
+            SampleFilters.getBlueMessFilter(),
+            SampleFilters.getLimeStutterFilter(),
+            SampleFilters.getStarLitFilter(),
+            SampleFilters.getNightWhisperFilter(),
+        )
+        val filterNameList = mutableListOf(
+            "필터 X",
+            "필터 O - Test1",
+            "필터 O - Test2",
+            "필터 O - Test3",
+            "필터 O - Test4",
+        )
+
+        getFilter { aiFilterList ->
+            callback(List(filterList.size + aiFilterList.size) { i ->
+                if (i < filterList.size) {
+                    FilterItem(
+                        filterNameList[i],
+                        mutableListOf(
+                            filterList[i]!!.processFilter(getImage1()),
+                            filterList[i]!!.processFilter(getImage2()),
+                            filterList[i]!!.processFilter(getImage3()),
+                            filterList[i]!!.processFilter(getImage4()),
+                        )
+                    )
+                } else {
+                    val aiIndex = i - filterList.size
+                    FilterItem(
+                        aiFilterList[aiIndex],
+                        mutableListOf(
+                            getImage1()!!,
+                            getImage2()!!,
+                            getImage3()!!,
+                            getImage4()!!,
+                        ),
+                        true
+                    )
+                }
+            })
+        }
+    }
+
+    private fun getFilter(callback: (List<String>) -> Unit) {
+        val retrofitAPI = RetrofitClient.getInstance().create(RetrofitAPI::class.java)
+        val call: Call<List<String>> = retrofitAPI.getFilter()
+
+        call.enqueue(object : Callback<List<String>> {
+            override fun onResponse(
+                call: Call<List<String>>,
+                response: Response<List<String>>
+            ) {
+                if (response.isSuccessful) {
+                    callback(response.body()!!)
+                }
+            }
+
+            override fun onFailure(call: Call<List<String>>, t: Throwable) {
+                Toast.makeText(this@FilterActivity, "네트워크 오류", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
 }

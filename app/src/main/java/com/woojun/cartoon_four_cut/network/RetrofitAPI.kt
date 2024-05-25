1e632114d2f5dc05a8761019b100c9effd4355a4
JangWoojun <woojun0107@naver.com>
2024-05-26 07:23:46 +0900

    feat: 백엔드 API 변경에 따른 요청 변경

--- app/src/main/java/com/woojun/cartoon_four_cut/network/RetrofitAPI.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/network/RetrofitAPI.kt
@@ -3,17 +3,15 @@ package com.woojun.cartoon_four_cut.network
 import com.woojun.cartoon_four_cut.BuildConfig
 import com.woojun.cartoon_four_cut.data.AuthResponse
 import com.woojun.cartoon_four_cut.data.FrameResponse
-import com.woojun.cartoon_four_cut.data.UploadRequest
-import com.woojun.cartoon_four_cut.data.UploadResponse
 import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Query
 
 
 interface RetrofitAPI {
@@ -23,14 +21,15 @@ interface RetrofitAPI {
     @GET("${BuildConfig.baseUrl}frame")
     suspend fun getFrame(): Response<List<FrameResponse>>
 
+    @GET("${BuildConfig.baseUrl}filter")
+    fun getFilter(): Call<List<String>>
+
     @Multipart
     @POST("${BuildConfig.baseUrl}upload")
     fun postUpload(
-        @Header("Header") header: UploadRequest,
+        @Header("auth") auth: String,
+        @Header("type") type: String,
         @Part body: List<MultipartBody.Part>,
-    ): Call<UploadResponse>
-
-    @GET("${BuildConfig.baseUrl}filter")
-    fun getFilter(): Call<List<String>>
+    ): Call<List<String>>
 
 }

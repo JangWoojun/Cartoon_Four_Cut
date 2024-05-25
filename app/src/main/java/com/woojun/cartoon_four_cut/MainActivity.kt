a921a49978298e988ae4104c35d04f4069dedeb4
JangWoojun <woojun0107@naver.com>
2024-05-26 08:29:41 +0900

    feat: 두번 뒤로가기 시 종료 추가

--- app/src/main/java/com/woojun/cartoon_four_cut/MainActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/MainActivity.kt
@@ -3,12 +3,17 @@ package com.woojun.cartoon_four_cut
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.woojun.cartoon_four_cut.adapter.HomePhotoFrameAdapter
 import com.woojun.cartoon_four_cut.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var backPressedTime: Long = 0
+    private val backPressInterval: Long = 2000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,5 +27,15 @@ class MainActivity : AppCompatActivity() {
 
         binding.photoRecyclerView.adapter = adapter
         binding.photoRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+    }
+
+    override fun onBackPressed() {
+        if (backPressedTime + backPressInterval > System.currentTimeMillis()) {
+            super.onBackPressed()
+            return
+        } else {
+            Toast.makeText(this, "한 번 더 누르면 종료됩니다", Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
     }
 }

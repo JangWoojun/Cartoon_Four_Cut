6f92fb8634ead3f0c73cb559a6299fd06c99365d
JangWoojun <woojun0107@naver.com>
2024-05-26 07:25:12 +0900

    feat: API 변경에 따른 속성 추가

--- app/src/main/java/com/woojun/cartoon_four_cut/data/FrameItem.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/data/FrameItem.kt
@@ -3,7 +3,8 @@ package com.woojun.cartoon_four_cut.data
 import android.graphics.Bitmap
 
 data class FrameItem (
-    val frame: FrameTypes,
-    val src: Int,
-    val images: MutableList<Bitmap>
+    val name: String,
+    val frameResponse: FrameResponse,
+    val images: MutableList<Bitmap>,
+    val isAi: Boolean = false
 )
--- app/src/main/java/com/woojun/cartoon_four_cut/data/FrameResponse.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/data/FrameResponse.kt
@@ -1,7 +1,12 @@
 package com.woojun.cartoon_four_cut.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class FrameResponse(
-    val background: Background,
-    val bottom: Bottom,
-    val top: Top
-)
+    val background: String,
+    val bottom: String,
+    val top: String,
+    val name: String
+): Parcelable

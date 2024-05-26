8ea100ad7a43d0bd721a29c7f6b78a49528c3f19
JangWoojun <woojun0107@naver.com>
2024-05-26 14:13:06 +0900

    feat: 백엔드 API 변경에 따른 요청 변경

--- app/src/main/java/com/woojun/cartoon_four_cut/network/RetrofitAPI.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/network/RetrofitAPI.kt
@@ -6,10 +6,12 @@ import com.woojun.cartoon_four_cut.data.FrameResponse
 import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Part
 import retrofit2.http.Query
 
@@ -25,11 +27,16 @@ interface RetrofitAPI {
     fun getFilter(): Call<List<String>>
 
     @Multipart
-    @POST("${BuildConfig.baseUrl}upload")
-    fun postUpload(
+    @PUT("${BuildConfig.baseUrl}image")
+    fun putImage(
         @Header("auth") auth: String,
         @Header("type") type: String,
         @Part body: List<MultipartBody.Part>,
+    ): Call<List<String>>
+
+    @POST("${BuildConfig.baseUrl}image")
+    fun postImage(
+        @Body body: List<String>,
     ): Call<List<String>>
 
 }

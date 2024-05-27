1c2d24362f7365c36926bd72fa463d10f2b9b8ad
JangWoojun <woojun0107@naver.com>
2024-05-27 10:06:35 +0900

    feat: 권한이 없을 시 설정창에서 요청하는 로직 추가 및 화면 전환 애니메이션 추가

--- app/src/main/java/com/woojun/cartoon_four_cut/CameraActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/CameraActivity.kt
@@ -2,9 +2,15 @@ package com.woojun.cartoon_four_cut
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.otaliastudios.cameraview.CameraListener
 import com.otaliastudios.cameraview.PictureResult
 import com.otaliastudios.cameraview.controls.Flash
@@ -19,11 +25,68 @@ class CameraActivity : AppCompatActivity() {
     private var isFlash = false
     private var imageCount = 0
 
+    companion object {
+        const val CAMERA_PERMISSION_CODE = 999
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
+        overridePendingTransition(R.anim.anim_slide_in_from_right_fade_in, R.anim.anim_fade_out)
         setContentView(binding.root)
 
+        checkCameraPermission()
+    }
+
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            startCamera()
+        } else {
+            requestCameraPermission()
+        }
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startCamera()
+                } else {
+                    Toast.makeText(this, "카메라 권한이 없어 사용하실 수 없습니다", Toast.LENGTH_SHORT).show()
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.CAMERA)) {
+                        requestCameraPermission()
+                    } else {
+                        getPermissionMove()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.anim_slide_in_from_left_fade_in, R.anim.anim_fade_out)
+    }
+
+    private fun getPermissionMove() {
+        Toast.makeText(this, "카메라 권한을 설정에서 허용해주세요", Toast.LENGTH_SHORT).show()
+
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivity(intent)
+    }
+
+
+    private fun startCamera() {
         binding.cameraView.setLifecycleOwner(this)
         binding.cameraView.addCameraListener(object : CameraListener() {
 
@@ -77,7 +140,7 @@ class CameraActivity : AppCompatActivity() {
         }
     }
 
-    fun cropBitmap(context: Context, bitmap: Bitmap): Bitmap {
+    private fun cropBitmap(context: Context, bitmap: Bitmap): Bitmap {
         val bitmapWidth = bitmap.width
         val startY = context.dpToPx(146.5f).toInt()
 

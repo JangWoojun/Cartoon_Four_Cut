7697c3b51c27c268dadba2c02f6c7b6443d04707
JangWoojun <woojun0107@naver.com>
2024-06-21 00:55:43 +0900

    remove: 필요 없는 코드 삭제

--- app/src/main/java/com/woojun/cartoon_four_cut/database/AppDatabase.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/database/AppDatabase.kt
@@ -7,10 +7,6 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.woojun.cartoon_four_cut.data.HomePhotoFrame
 
-@TypeConverters(
-    TypeConverter::class
-)
-
 @Database(entities = [HomePhotoFrame::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun homePhotoFrameItemDao(): HomePhotoFrameDAO

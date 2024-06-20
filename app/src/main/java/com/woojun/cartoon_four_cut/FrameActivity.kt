55756bd0e993779198e8a1356a914aaea3f8f765
JangWoojun <woojun0107@naver.com>
2024-06-21 00:49:07 +0900

    refactor: 필요없는 import 삭제

--- app/src/main/java/com/woojun/cartoon_four_cut/FrameActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/FrameActivity.kt
@@ -5,7 +5,6 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity

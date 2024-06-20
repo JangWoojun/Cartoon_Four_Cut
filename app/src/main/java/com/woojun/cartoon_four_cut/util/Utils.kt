e0e6ef682c79a16db84eb10dc2413b7701876c1e
JangWoojun <woojun0107@naver.com>
2024-06-21 00:41:04 +0900

    feat: Util 함수 추가
    파일을 Bitmap으로 바꾸는 함수 추가

--- app/src/main/java/com/woojun/cartoon_four_cut/util/Utils.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/util/Utils.kt
@@ -1,9 +1,22 @@
 package com.woojun.cartoon_four_cut.util
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.io.File
 
 object Utils {
     fun Context.dpToPx(dp: Float): Float {
         return dp * this.resources.displayMetrics.density
+    }
+
+    fun loadImageFromFilePath(filePath: String): Bitmap? {
+        val file = File(filePath)
+
+        return if (file.exists()) {
+            BitmapFactory.decodeFile(file.absolutePath)
+        } else {
+            null
+        }
     }
 }

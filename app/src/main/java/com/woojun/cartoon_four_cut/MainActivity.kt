6abe6988f8ca30ce31beea59f32eb99f66079d6f
JangWoojun <woojun0107@naver.com>
2024-06-21 00:48:43 +0900

    feat: MainActivity 이미지 없을 시 보여줄 View 가시성 로직 추가

--- app/src/main/java/com/woojun/cartoon_four_cut/MainActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut/MainActivity.kt
@@ -6,17 +6,17 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.woojun.cartoon_four_cut.adapter.DefaultHomePhotoFrameAdapter
 import com.woojun.cartoon_four_cut.adapter.HomePhotoFrameAdapter
 import com.woojun.cartoon_four_cut.database.AppDatabase
 import com.woojun.cartoon_four_cut.databinding.ActivityMainBinding
 import com.woojun.cartoon_four_cut.util.OnSingleClickListener
+import com.woojun.cartoon_four_cut.util.VisibilityControlListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), VisibilityControlListener {
     private lateinit var binding: ActivityMainBinding
 
     private var backPressedTime: Long = 0
@@ -37,15 +37,17 @@ class MainActivity : AppCompatActivity() {
             val homePhotoFrameDao = AppDatabase.getDatabase(this@MainActivity)?.homePhotoFrameItemDao()
             val list = homePhotoFrameDao!!.getHomePhotoFrameList()
 
-            val mainAdapter = HomePhotoFrameAdapter(list.reversed().toMutableList())
-            val defaultAdapter = DefaultHomePhotoFrameAdapter(getDefaultList())
+            val mainAdapter = HomePhotoFrameAdapter(list.reversed().toMutableList(), this@MainActivity)
 
             withContext(Dispatchers.Main) {
                 if (list.size > 0) {
-                    binding.photoRecyclerView.adapter = mainAdapter
+                    binding.defaultImage.visibility = View.GONE
+                    binding.photoRecyclerView.visibility = View.VISIBLE
                 } else {
-                    binding.photoRecyclerView.adapter = defaultAdapter
+                    binding.defaultImage.visibility = View.VISIBLE
+                    binding.photoRecyclerView.visibility = View.GONE
                 }
+                binding.photoRecyclerView.adapter = mainAdapter
                 binding.photoRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
             }
         }
@@ -60,38 +62,22 @@ class MainActivity : AppCompatActivity() {
                     val homePhotoFrameDao = AppDatabase.getDatabase(this@MainActivity)?.homePhotoFrameItemDao()
                     val list = homePhotoFrameDao!!.getHomePhotoFrameList()
 
-                    val mainAdapter = HomePhotoFrameAdapter(list.reversed().toMutableList())
-                    val defaultAdapter = DefaultHomePhotoFrameAdapter(getDefaultList())
+                    val mainAdapter = HomePhotoFrameAdapter(list.reversed().toMutableList(), this@MainActivity)
 
                     withContext(Dispatchers.Main) {
                         if (list.size > 0) {
-                            binding.photoRecyclerView.adapter = mainAdapter
+                            binding.defaultImage.visibility = View.GONE
+                            binding.photoRecyclerView.visibility = View.VISIBLE
                         } else {
-                            binding.photoRecyclerView.adapter = defaultAdapter
+                            binding.defaultImage.visibility = View.VISIBLE
+                            binding.photoRecyclerView.visibility = View.GONE
                         }
+                        binding.photoRecyclerView.adapter = mainAdapter
                         binding.photoRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
                     }
                 }
             }
         }
-    }
-
-    private fun getDefaultList(): List<List<Int>> {
-
-        return listOf(
-            listOf(
-                R.drawable.img1,
-                R.drawable.img3,
-                R.drawable.img2,
-                R.drawable.img4
-            ),
-            listOf(
-                R.drawable.img5,
-                R.drawable.img7,
-                R.drawable.img6,
-                R.drawable.img8
-            )
-        )
     }
 
     override fun onBackPressed() {
@@ -102,5 +88,15 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "한 번 더 누르면 종료됩니다", Toast.LENGTH_SHORT).show()
         }
         backPressedTime = System.currentTimeMillis()
+    }
+
+    override fun onVisibilityChange(visible: Boolean) {
+        if (!visible) {
+            binding.defaultImage.visibility = View.GONE
+            binding.photoRecyclerView.visibility = View.VISIBLE
+        } else {
+            binding.defaultImage.visibility = View.VISIBLE
+            binding.photoRecyclerView.visibility = View.GONE
+        }
     }
 }
